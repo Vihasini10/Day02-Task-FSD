8862aa456d88a0fdccce0c2cb07cd87242aacb17
Vihasini10 <vihasinisathish@gmail.com>
2025-06-12 14:43:06 +0530

    Add files via upload

---
+++ java/AddressBook.java
@@ -0,0 +1,32 @@
+package basicpackage.java;
+import java.util.Scanner;
+
+public class AddressBook {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("How many contacts do you want to add? ");
+        int n = scanner.nextInt();
+        scanner.nextLine(); // consume newline
+
+        String[] names = new String[n];
+        String[] phones = new String[n];
+
+        // Input contacts using for loop
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter details for contact #" + (i + 1));
+            System.out.print("Name: ");
+            names[i] = scanner.nextLine();
+            System.out.print("Phone: ");
+            phones[i] = scanner.nextLine();
+        }
+
+        // Display contacts
+        System.out.println("\n--- Your Contacts ---");
+        for (int i = 0; i < n; i++) {
+            System.out.println((i + 1) + ". " + names[i] + " - " + phones[i]);
+        }
+
+        scanner.close();
+    }
+}
